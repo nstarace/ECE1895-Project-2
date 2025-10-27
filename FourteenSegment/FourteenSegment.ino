7be027246e24f62099a90009908f8916fa88143a
Nicholas <nstarace8@gmail.com>
2025-10-27 18:27:13 -0400

    Updated FourteenSegment

--- FourteenSegment/FourteenSegment.ino
+++ FourteenSegment/FourteenSegment.ino
@@ -1,3 +1,5 @@
+// Simply call incrementScore()
+
 const uint8_t a1 = 41;
 const uint8_t b1 = 1;
 const uint8_t c1 = 2;
@@ -9,6 +11,8 @@ const uint8_t c2 = 39;
 const uint8_t d2 = 38;
 
 const uint8_t ScoreDisplay[8] = {a2, b2, c2, d2, a1, b1, c1, d1};
+byte currScore = 0x00;
+
 
 void setup() {
   for (int i = 0; i < 8; i++) {
@@ -16,11 +20,18 @@ void setup() {
   }
 }
 
-void loop() {
-  byte Score = 0x00;
+void incrementScore() {
+  currScore++;
   for (int i = 0; i < 8; i++) {
-    bool bit = (Score >> i) & 1;
+    bool bit = (currScore >> i) & 1;
     digitalWrite(ScoreDisplay[i], bit ? HIGH : LOW);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+    incrementScore();
+    delay(1000);
   }
 }
 
